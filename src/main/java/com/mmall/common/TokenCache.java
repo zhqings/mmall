4f08a36959ce2aa6056d92e922d53fd583c711b0
DESKTOP-E5L6SCF <298080741@qq.com>
2019-06-06 17:37:29 +0800

    1.0.0

--- src/main/java/com/mmall/common/TokenCache.java
+++ src/main/java/com/mmall/common/TokenCache.java
@@ -17,7 +17,7 @@ public class TokenCache {
 
     public static final String TOKEN_PREFIX = "token_";
 
-    //LRU算法
+    //LRU算法，初始化缓存空间，设置最大值，设置有效期
     private static LoadingCache<String,String> localCache = CacheBuilder.newBuilder().initialCapacity(1000).maximumSize(10000).expireAfterAccess(12, TimeUnit.HOURS)
             .build(new CacheLoader<String, String>() {
                 //默认的数据加载实现,当调用get取值的时候,如果key没有对应的值,就调用这个方法进行加载.

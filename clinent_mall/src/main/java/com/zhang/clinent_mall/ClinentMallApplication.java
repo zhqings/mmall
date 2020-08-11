382997f87a52b7d9147dfc1e64e3e9c0c9810d93
zhang <zhangtopsun@163.com>
2020-08-11 12:00:25 +0800

    新增程序

---
+++ clinent_mall/src/main/java/com/zhang/clinent_mall/ClinentMallApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.clinent_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinentMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinentMallApplication.class, args);
+    }
+
+}
---
+++ clinent_mall/src/test/java/com/zhang/clinent_mall/ClinentMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.clinent_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinentMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

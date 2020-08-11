09081aa048bdde8ea6ca2b175d395291264b6146
haiqing.zhang <298080741@qq.com>
2020-08-11 18:03:45 +0800

    请求缓存cache中存储用户信息

--- client_mall/src/main/java/com/zhang/client_mall/controller/UserApi.java
+++ client_mall/src/main/java/com/zhang/client_mall/controller/UserApi.java
@@ -4,6 +4,8 @@ import com.zhang.client_mall.common.Constant;
 import com.zhang.client_mall.common.ServerResponse;
 import com.zhang.client_mall.common.SystemCode;
 import com.zhang.client_mall.dto.UserDTO;
+import com.zhang.client_mall.util.MD5Util;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +14,13 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/user")
+@Slf4j
 public class UserApi {
 
     @PostMapping("/login")
     public ServerResponse login(UserDTO userDTO, HttpSession session){
+        log.info("md5加密：{}", MD5Util.MD5EncodeUtf8(userDTO.getPhone()));
+
         if (Constant.LOGIN.PHONE.equals(userDTO.getPhone())&&Constant.LOGIN.PASSWORD.equals(userDTO.getPassword())){
             session.setAttribute("user",userDTO);
             return ServerResponse.success();

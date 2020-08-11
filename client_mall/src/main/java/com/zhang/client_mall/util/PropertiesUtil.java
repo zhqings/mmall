8337faadf4f0def54612a89603296d692c25524d
zhang <zhangtopsun@163.com>
2020-08-11 17:28:16 +0800

    商城新增

---
+++ client_mall/src/main/java/com/zhang/client_mall/util/PropertiesUtil.java
@@ -0,0 +1,50 @@
+package com.zhang.client_mall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * @author zhang
+ * @date 2020/8/11 16:08
+ * @descript 获取指定文件的参数
+ */
+public class PropertiesUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+
+    private static Properties props;
+
+    static {
+        String fileName = "application.properties";
+        props = new Properties();
+        try {
+            props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName), "UTF-8"));
+        } catch (IOException e) {
+            logger.error("配置文件读取异常", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        String value = props.getProperty(key.trim());
+        if (StringUtils.isBlank(value)) {
+            return null;
+        }
+        return value.trim();
+    }
+
+    public static String getProperty(String key, String defaultValue) {
+
+        String value = props.getProperty(key.trim());
+        if (StringUtils.isBlank(value)) {
+            value = defaultValue;
+        }
+        return value.trim();
+    }
+
+
+}

1a31bbd346bca76c83cc749187a59fc470995bac
zhang <zhangtopsun@163.com>
2020-09-10 19:48:24 +0800

    启动注解

--- client_mall/src/main/java/com/zhang/client_mall/ClientMallApplication.java
+++ client_mall/src/main/java/com/zhang/client_mall/ClientMallApplication.java
@@ -2,10 +2,14 @@ package com.zhang.client_mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ApplicationContextEvent;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.zhang.client_mall.servlet")
 public class ClientMallApplication {
-
+ApplicationContextEvent
     public static void main(String[] args) {
         SpringApplication.run(ClientMallApplication.class, args);
     }
